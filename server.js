0ea58d1d7f95d36f81a60fcba7902ab88224093d
Hudson Dorward <27364005@edu.cbe.ab.ca>
2024-10-23 15:22:28 -0600

    made progress in guess

--- server.js
+++ server.js
@@ -26,9 +26,38 @@ server.get('/newgame',function(req,res){
 server.get('/gamestate', function(req,res){
     sessionID = req.query.sessionID
     gameState = activeSessions[sessionID]
-    console.log(gameState)
     res.status(200) 
     res.send({gameState: gameState})
+})
+server.post('/guess', function(req,res){
+    sessionID = req.body.sessionID
+    guess = req.body.guess
+    gameState = activeSessions[sessionID]
+    console.log(gameState)
+    if(gameState.remainingGuesses > 0){
+        answer = gameState.wordToGuess.split("") 
+        attempt = guess.split("")
+        if(answer == attempt){
+            console.log("YAYYYYYY")
+        } else {
+            for(let i=0; i <5;i++){
+                console.log(answer[i] + " ---- "+ attempt[i])
+                if(answer[i] == attempt[i]){
+                    gameState.rightLetters.push(answer[i])
+                } else {
+                    for(let i2 = 0; i2<5;i2++){
+                        if(answer[i2] == attempt[i2]){
+                            gameState.closeLetters.push(answer[i])
+                        }
+                    }
+                }
+            }
+        }
+        console.log(answer)
+        console.log(attempt)
+        console.log(gameState)
+    }
+    res.status(201)
 })
 
 
